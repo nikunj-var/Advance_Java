ec9d83732da8aa11db006207acec5deab5c31ca5
“nikunj-var” <nikunjvar2002@gmail.com>
2024-07-06 22:48:09 +0530

    added authentication

--- SpringBoot/customer-management/src/main/java/com/cetpa/controllers/CustomerController.java
+++ SpringBoot/customer-management/src/main/java/com/cetpa/controllers/CustomerController.java
@@ -11,13 +11,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.cetpa.entities.Customer;
 import com.cetpa.service.CustomerService;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class CustomerController {
 	@Autowired private CustomerService customerService;
 	
 	@RequestMapping("customer-management")
-	public String getHomeView() {
+	public String getHomeView(HttpSession session) {
+		if(session.getAttribute("user")==null)
+			return "login";
+		
 		return "home";
+	}
+	
+	@RequestMapping("customer-management/login/validate")
+	public String validatelogin(String uid,String pass,HttpSession session,Model model) {
+		System.out.println(uid+pass);
+		if(uid.equals("admin")) {
+			if(pass.equals("cetpa")) {
+				session.setAttribute("user", "admin");
+				return "home";
+			}
+			else {
+				model.addAttribute("msg","Entered password is wrong");
+			}
+		}
+		else {
+			model.addAttribute("msg","Entered user id does not wrong");
+		}
+		return "login";
+		
+	}
+	
+	@RequestMapping("logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "login";
 	}
 	
 	@RequestMapping("customer-management/add")
--- SpringBoot/customer-management/src/main/java/com/cetpa/repositories/CustomerRepository.java
+++ SpringBoot/customer-management/src/main/java/com/cetpa/repositories/CustomerRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.cetpa.entities.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer,Integer>{
-
+	
 }
